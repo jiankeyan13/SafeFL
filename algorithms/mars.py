e9b37e42243ce91d675af5b2a31d5861686cace3
jiankeyan13 <649471913@qq.com>
2026-05-09 16:45:39 +0800

    删除文件

--- algorithms/mars.py
+++ algorithms/mars.py
@@ -4,21 +4,22 @@ from core.client.base_client import BaseClient
 from core.server.aggregator.avg_aggregator import AvgAggregator
 from core.server.base_server import BaseServer
 from core.server.refiner.base_refiner import BaseRefiner
-from core.server.screener.mars import MARSScreener
+from core.server.screener.mars import MARSGammaBnStatScreener
 from core.utils.registry import ALGORITHM_REGISTRY
 
 
-@ALGORITHM_REGISTRY.register("mars")
-def build_mars_algorithm(
+@ALGORITHM_REGISTRY.register("mars_normal")
+def build_mars_normal_algorithm(
     model, device, config: dict, seed: int, **params
 ) -> Tuple[BaseServer, Type[BaseClient]]:
     """
-    MARS: MARS 筛选 + 平均聚合 + 标准模型更新。
+    MarsNormal: 基于 MARSGammaBnStatScreener 的 MARS, 每通道缩放置为 |gamma| * std.
+    其余流程 (CBE, Wasserstein, K-Means) 与 mars 相同.
     """
     del config
     screener_conf = params.get("screener", {})
     screener_params = screener_conf.get("params", screener_conf)
-    screener = MARSScreener(seed=seed, **screener_params)
+    screener = MARSGammaBnStatScreener(seed=seed, **screener_params)
 
     aggregator = AvgAggregator(device=device)
     refiner = BaseRefiner(config=params.get("refiner", {}))
